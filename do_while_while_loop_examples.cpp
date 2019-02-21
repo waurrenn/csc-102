eb974e489ffab571b334322afb25229834f49679
Lauren Heng <43734966+waurrenn@users.noreply.github.com>
2019-02-20 21:29:14 -0500

    Update do_while_while_loop_examples.cpp

--- do_while_while_loop_examples.cpp
+++ do_while_while_loop_examples.cpp
@@ -10,14 +10,8 @@ int main()
 		cout<<i<<endl;
 		i++; 			//increment
 	}
-	//program will continue here after the loop breaks
-	cout<<"Last value of i: "<<i<<endl;	//last value of i 
-	 
-	//init i again to 0 for the do while loop
+	cout<<"Last value of i: "<<i<<endl;	
 	i=0;
-	//the breaking condition is at the end and loop verifies the condition at 
-	//the end of each iteration, loop will run regardless of any conditions
-	//for the first iteration
 	do
 	{
 		cout<<i<<endl;
