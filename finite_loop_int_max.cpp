daa7dc4acd76fa4dfe0da73ffcb1baa06f407d98
Lauren Heng <43734966+waurrenn@users.noreply.github.com>
2019-02-20 21:26:25 -0500

    Update finite_loop_int_max.cpp

--- finite_loop_int_max.cpp
+++ finite_loop_int_max.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()				
 {
 	int i;
-	// value of i will change to negetive after 2147483648(max) 
+	// value of i will change to negative after 2147483648(max) 
 	//This is an finite loop
 	for(i=1;i>0;i=i+99999)		//loop will break when the value is negetive
 	{
