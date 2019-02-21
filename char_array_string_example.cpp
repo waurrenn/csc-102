dc72091b82fea1e1a4699013cde6544b49ac8ab7
Lauren Heng <43734966+waurrenn@users.noreply.github.com>
2019-02-20 21:28:34 -0500

    Update char_array_string_example.cpp

--- char_array_string_example.cpp
+++ char_array_string_example.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main()				
 {
 	int i;
-	//See details about the ASCII table and values in the book or any online source
-	//This program will print some additional symbols
 	char c;
-	c='A';		//Starts from A  65
-	while(c<='z')	//Ends at z 122
+	c='A';	
+	while(c<='z')	
 	{
-		i=c;	//assign the char value to the integer varriable
-		cout<<c<<" ";			//print the character
-		cout<<i<<endl;		//print the ASCII value
-		c++;			//Increment by one, goes to the next character
+		i=c;	
+		cout<<c<<" ";			
+		cout<<i<<endl;		
+		c++;			
 	}
 	return 0;
 }
